060a82ccb061f5a4d81b7ca7abedc0deec232830
rasmus842 <rasmuszalite@gmail.com>
2022-06-10 19:01:10 +0300

    Finished 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  let tempInCelcius = (tempInFahrenheit - 32) * 5/9;
+  return Number(tempInCelcius.toFixed(1));
 };
 
-const ctof = function() {
 
+const ctof = function(tempInCelcius) {
+  let tempInFahrenheit = tempInCelcius * 9/5 + 32;
+  return Number(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
