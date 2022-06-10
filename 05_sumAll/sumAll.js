18977af36d3b5d93667640f242b61de2015a418f
rasmus842 <rasmuszalite@gmail.com>
2022-06-10 18:42:37 +0300

    Finished 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    if (typeof firstNumber != "number" || typeof secondNumber != "number") {
+        return "ERROR";
+    }
+
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+
+    if (firstNumber > secondNumber) {
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+
+    let sum = 0;
+    for(let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
