9fba2dd861f04e1c87fb998bb14789b148776b6f
rasmus842 <rasmuszalite@gmail.com>
2022-06-10 17:44:24 +0300

    Finished 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if (times < 0) return 'ERROR';
 
+    let concatString = '';
+    for(let i = 0; i < times; i++) {
+        concatString += str;
+    }
+    return concatString;
 };
 
 // Do not edit below this line
