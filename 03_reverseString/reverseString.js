d37a8337e55532ada6f92af7f48023c06a854426
rasmus842 <rasmuszalite@gmail.com>
2022-06-10 17:56:52 +0300

    Finished 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textAsArray = text.split('');
+    let reversed = textAsArray.reverse().join('');
+    return reversed;
 };
 
 // Do not edit below this line
